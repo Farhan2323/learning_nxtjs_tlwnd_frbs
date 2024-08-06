04cdb95c9e0b2bff9ba2540cb35b10ddaf295c4a
Farhan2323 <89047823+Farhan2323@users.noreply.github.com>
2024-08-06 14:58:49 -0500

    Update firebase.js

--- app/firebase.js
+++ app/firebase.js
@@ -3,16 +3,7 @@ import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 import { getFirestore } from "firebase/firestore";
-
-// Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyCOGDKbwwXYOFDtGXNeIDZu6avf2l9o9rg",
-  authDomain: "learn-nxtjs-twind-fbase.firebaseapp.com",
-  projectId: "learn-nxtjs-twind-fbase",
-  storageBucket: "learn-nxtjs-twind-fbase.appspot.com",
-  messagingSenderId: "497236594454",
-  appId: "1:497236594454:web:51e95d451ee5c8171f0a37"
-};
+//// FIREBASE API KEYS REMOVED FOR SECURITY REASONS.
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
